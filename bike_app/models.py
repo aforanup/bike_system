b72ec196af152f74796e53a689ff9fa3a13d0451
aforanup <aforanupn@gmail.com>
2022-02-19 21:15:32 +0545

    models added

--- bike_app/models.py
+++ bike_app/models.py
@@ -1,3 +1,41 @@
 from django.db import models
 
-# Create your models here.
+
+class BikeModel(models.Model):
+    name = models.CharField(max_length=255)
+    image = models.ImageField(upload_to="bike_image/")
+
+    class meta:
+        verbose_name = "Bike"
+        verbose_name_plural = "Bikes"
+
+    def __str__(self):
+        return self.name
+
+
+class BikeDetailModel(models.Model):
+    bike = models.OneToOneField(
+        BikeModel, related_name="bike_model", on_delete=models.CASCADE
+    )
+    description = models.TextField()
+
+    class meta:
+        verbose_name = "Bike Detail"
+        verbose_name_plural = "Bike Details"
+
+    def __str__(self):
+        return self.bike.name
+
+
+class BikeImageModel(models.Model):
+    bike_model = models.ForeignKey(
+        BikeModel, related_name="bike_images", on_delete=models.CASCADE, blank=True
+    )
+    image = models.ImageField(upload_to='bike_image/')
+
+    class meta:
+        verbose_name = "Bike Image"
+        verbose_name_plural = "Bike Images"
+
+    def __str__(self):
+        return self.bike.name
