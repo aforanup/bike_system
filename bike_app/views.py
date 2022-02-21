528ffe26394414e4512564d543f2ab88ce4ad975
aforanup <aforanupn@gmail.com>
2022-02-21 15:06:58 +0545

    detail page setup

--- bike_app/views.py
+++ bike_app/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from . models import BikeModel, BikeDetailModel, BikeImageModel
-from django.views.generic import ListView, DetailView
+from django.views.generic import ListView, DetailView, UpdateView, DeleteView, CreateView
 
 
 class BikeListView(ListView):
@@ -21,7 +21,30 @@ class BikeDetailView(DetailView):
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        context['details'] = bikeDetailModel.objects.filter(
-            pk=self.kwargs['pk']
+        print("aaa", self.kwargs['pk'])
+        context['details'] = BikeDetailModel.objects.get(
+            bike=int(self.kwargs['pk'])
         )
+        context['images'] = BikeImageModel.objects.filter(
+            bike_model=int(self.kwargs['pk'])
+        )
+        print(context['details'], 'bbbb')
         return context
+
+
+class DetailUpdateView(UpdateView):
+    model = BikeModel
+    paginated_by = 4
+    template_name = 'bike_app/home.html'
+
+
+class BikeDeleteView(DeleteView):
+    model = BikeModel
+    paginated_by = 4
+    template_name = 'bike_app/home.html'
+
+
+class BikeCreateView(CreateView):
+    model = BikeModel
+    paginated_by = 4
+    template_name = 'bike_app/home.html'
